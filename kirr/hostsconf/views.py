9bcd7907341a00891750b5128a857f07940e336c
Darvin <darvin_palma@yahoo.com>
2017-04-11 14:22:42 -0400

    Updated hostconf/views.py

--- kirr/hostsconf/views.py
+++ kirr/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "http://www.anythingphilippines.com")
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "https://darvinurlshort.herokuapp.com")
 
 def wildcard_redirect(request, path=None):
 	new_url = DEFAULT_REDIRECT_URL
